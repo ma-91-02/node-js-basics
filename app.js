dabc7f3973714e74c9e480d80d20cc3f03f13f55
ma-91-02 <ma.91.02@mail.ru>
2022-04-17 21:44:11 +0300

    end section 3

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  const url = req.url;
-  if (url === '/'){
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"></body></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
