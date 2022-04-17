9915cbe5d7ce1ae1fdbc5f45803437d54951e065
ma-91-02 <ma.91.02@mail.ru>
2022-04-17 23:06:35 +0300

    section 4

--- routes.js
+++ routes.js
@@ -31,7 +31,7 @@ const requestHandler = (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("<body><h1>Hello from my Node.js server live</h1></body>");
   res.write("</html>");
   res.end();
 };
