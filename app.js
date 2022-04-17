c8f0aeb530f9370b0b2b98b264af9e9bdc13f20e
ma-91-02 <ma.91.02@mail.ru>
2022-04-17 19:11:23 +0300

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  if (url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"></body></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
